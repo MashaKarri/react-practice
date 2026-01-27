46466f885f8ddc384a379f301597b42961d2ef0f
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2026-01-27 21:01:45 +0200

    react-1

---
+++ src/components/Article.jsx
@@ -0,0 +1,13 @@
+export const Article = () => {
+  return (
+    <article>
+      <h2>React</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi mollitia
+        sapiente inventore numquam voluptate nulla quos fugit architecto
+        nesciunt quas dolores illo quae, suscipit alias quia iusto enim delectus
+        neque.
+      </p>
+    </article>
+  );
+};
