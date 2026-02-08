d5c3e327bfb196d9f86058478f5fbb06c1796b0b
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2026-02-08 11:45:48 +0200

    react-2

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,16 @@
 export const Article = () => {
   return (
-    <article>
-      <h2>React</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi mollitia
-        sapiente inventore numquam voluptate nulla quos fugit architecto
-        nesciunt quas dolores illo quae, suscipit alias quia iusto enim delectus
-        neque.
-      </p>
-    </article>
+    <>
+      <h1>Test</h1>
+      <article>
+        <h2>React</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi
+          mollitia sapiente inventore numquam voluptate nulla quos fugit
+          architecto nesciunt quas dolores illo quae, suscipit alias quia iusto
+          enim delectus neque.
+        </p>
+      </article>
+    </>
   );
 };
